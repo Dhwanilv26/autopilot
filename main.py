e44dd5c6dae531353c769f6e50652bd1325ddbf3
Dhwanilv26 <dhwanil2604@gmail.com>
2026-02-09 23:40:08 +0530

    completed the streaming and non streaming response succesfully and understood coroutines and async for, and async generator properly

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ import asyncio
 async def main():
     client = LLMClient()
     messages = [{"role": "user", "content": "what is up"}]
-    await client.chat_completion(messages, stream=False)
+    # yield value in the event variable
+
+    # used async for instead of await to process the response in chunks and not wait till the entire response, and chat_completion returns an async generator
+    async for event in client.chat_completion(messages, stream=True):
+        print(event)
+
     print("done")
 
+
+# python is a synchronous language, we need asyncio for the main function to run
 asyncio.run(main())
