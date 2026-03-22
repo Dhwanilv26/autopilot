b8fa807a0b5caf36266076e23bea5bfe07502f1f
Dhwanilv26 <dhwanil2604@gmail.com>
2026-03-22 13:30:30 +0530

    successfully tested with test_tool.py and tool discovery also works, LETS MOVE TO MCPPP!!

--- tools/discovery.py
+++ tools/discovery.py
@@ -7,6 +7,10 @@ from config.config import Config
 from config.loader import get_config_dir
 from tools.base import Tool
 from tools.registry import ToolRegistry
+# modules are just objects stored in memory (sys.modules)
+# files are treated as modules, and folders as packages in python
+# python finds file in current folder and sys.path,
+# loads and executes it, stores in sys.modules and returns the module
 
 
 class ToolDiscoveryManager:
@@ -16,36 +20,40 @@ class ToolDiscoveryManager:
 
     def _load_tool_modules(self, file_path: Path) -> Any:
         module_name = f'discovered_tool_{file_path.stem}'
+        # spec -> instructions on how to load this file as module
         spec = importlib.util.spec_from_file_location(module_name, file_path)
 
         if spec is None or spec.loader is None:
             return ImportError(f"could not load spec from {file_path}")
 
+        # creates empty module object
         module = importlib.util.module_from_spec(spec)
         sys.modules[module_name] = module
-
+        # runs the file
         spec.loader.exec_module(module)
 
         return module
 
-    def _find_tool_classes(self, module: Any) -> list[Tool]:
-        tools: list[Tool] = []
+    def _find_tool_classes(self, module: Any) -> list[type[Tool]]:
+        tools: list[type[Tool]] = []
 
         for name in dir(module):
+            # same as module.MyTool
             obj = getattr(module, name)
 
-            if inspect.isclass(obj) and issubclass(obj, Tool) and obj is not Tool and obj.__module__ == module.name:
+            # only pick tool classes that are present in this module without the base class
+            if inspect.isclass(obj) and issubclass(obj, Tool) and obj is not Tool and obj.__module__ == module.__name__:
                 tools.append(obj)
 
         return tools
 
-    def discovery_from_directory(self, directory: Path) -> None:
+    def discover_from_directory(self, directory: Path) -> None:
         tool_dir = directory/'.agentic-cli'/'tools'
 
         if not tool_dir.exists() or not tool_dir.is_dir():
             return
 
-        for py_file in tool_dir.glob('**/.py'):
+        for py_file in tool_dir.glob('**/*.py'):
             try:
                 if py_file.name.startswith("__"):
                     continue
@@ -63,4 +71,4 @@ class ToolDiscoveryManager:
 
     def discover_all(self) -> None:
         self.discover_from_directory(self.config.cwd)
-        self.discovery_from_directory(get_config_dir())
+        self.discover_from_directory(get_config_dir())
